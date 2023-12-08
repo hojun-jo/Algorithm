1f46441d6cf56fcaa2c288ad5788d6e603e48b9b
Moon <86751964+hojun-jo@users.noreply.github.com>
2023-12-08 15:20:45 +0900

    Create 올바른 괄호.swift

---
+++ Programmers/Lv.2/올바른 괄호.swift
@@ -0,0 +1,15 @@
+func solution(_ s:String) -> Bool {
+    var stack = [Character]()
+    
+    for char in s {
+        if char == "(" {
+            stack.append(char)
+        } else if stack.isEmpty {
+            return false
+        } else {
+            stack.removeLast()
+        }
+    }
+    
+    return stack.isEmpty
+}
