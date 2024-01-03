d3f7491f41dad8b4d7280963faab6b77481aa6a0
Moon <86751964+hojun-jo@users.noreply.github.com>
2024-01-03 16:20:10 +0900

    Create 15656 N과 M (7).swift

---
+++ Baekjoon/15656 N과 M (7).swift
@@ -0,0 +1,21 @@
+let nm = readLine()!.split(separator: " ").map { Int($0)! }
+let (n, m) = (nm[0], nm[1])
+let numbers = readLine()!.split(separator: " ").map { Int($0)! }.sorted().map(String.init)
+var stack = [String]()
+var result = String()
+
+backtracking()
+print(result)
+
+func backtracking() {
+    guard stack.count < m else {
+        result += "\(stack.joined(separator: " "))\n"
+        return
+    }
+    
+    for number in numbers {
+        stack.append(number)
+        backtracking()
+        stack.removeLast()
+    }
+}
