1c0705e0cdda8f4681527d13bd6d141c28b6d96f
Moon <86751964+hojun-jo@users.noreply.github.com>
2024-04-05 00:35:06 +0900

    Create 2252 줄 세우기.swift

---
+++ Baekjoon/2252 줄 세우기.swift
@@ -0,0 +1,46 @@
+struct Queue<T> {
+    private var q = [T]()
+    private var head = 0
+    
+    var isEmpty: Bool {
+        return q.count - head == 0
+    }
+    
+    mutating func enqueue(_ element: T) {
+        q.append(element)
+    }
+    
+    mutating func dequeue() -> T? {
+        guard self.isEmpty == false else { return nil }
+        head += 1
+        return q[head - 1]
+    }
+}
+
+let nm = readLine()!.split(separator: " ").map { Int($0)! }
+let n = nm[0], m = nm[1]
+var edges = Array(repeating: [Int](), count: n + 1)
+var degrees = Array(repeating: 0, count: n + 1)
+var q = Queue<Int>()
+var result = String()
+
+for _ in 1...m {
+    let input = readLine()!.split(separator: " ").map { Int($0)! }
+    edges[input[0]].append(input[1])
+    degrees[input[1]] += 1
+}
+
+for i in 1...n {
+    if degrees[i] == 0 { q.enqueue(i) }
+}
+
+while let current = q.dequeue() {
+    result += "\(current) "
+    
+    for next in edges[current] {
+        degrees[next] -= 1
+        if degrees[next] == 0 { q.enqueue(next) }
+    }
+}
+
+print(result)
