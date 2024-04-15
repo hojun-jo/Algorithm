6d79f995fff85bf74747f335f2627439a535c590
Moon <86751964+hojun-jo@users.noreply.github.com>
2024-04-16 01:42:10 +0900

    Create 1753 최단경로.swift

---
+++ Baekjoon/1753 최단경로.swift
@@ -0,0 +1,100 @@
+struct Heap<T> {
+  private var heap = [T]()
+  private let comparer: (T, T) -> Bool
+  
+  var isEmpty: Bool {
+    return heap.isEmpty
+  }
+  
+  var peek: T? {
+    return heap.first
+  }
+  
+  init(comparer: @escaping (T, T) -> Bool) {
+    self.comparer = comparer
+  }
+  
+  mutating func push(_ data: T) {
+    heap.append(data)
+    moveUp(from: heap.count - 1)
+  }
+  
+  mutating func pop() -> T? {
+    let count = heap.count
+    guard count > 0 else { return nil }
+    heap.swapAt(0, count - 1)
+    let deletedData = heap.removeLast()
+    moveDown(from: 0)
+    return deletedData
+  }
+  
+  mutating func removeAll() {
+    heap.removeAll()
+  }
+  
+  mutating private func moveUp(from index: Int) {
+    var index = index
+    while index > 0 &&
+            comparer(heap[index], heap[(index - 1) / 2]) {
+      heap.swapAt(index, (index - 1) / 2)
+      index = (index - 1) / 2
+    }
+  }
+  
+  mutating private func moveDown(from index: Int) {
+    var currentIndex = index
+    var isSwap = false
+    let leftChildIndex = currentIndex * 2 + 1
+    let rightChildIndex = currentIndex * 2 + 2
+    
+    if leftChildIndex < heap.endIndex &&
+        comparer(heap[leftChildIndex], heap[currentIndex]) {
+      currentIndex = leftChildIndex
+      isSwap = true
+    }
+    
+    if rightChildIndex < heap.endIndex &&
+        comparer(heap[rightChildIndex], heap[currentIndex]) {
+      currentIndex = rightChildIndex
+      isSwap = true
+    }
+    
+    if isSwap {
+      heap.swapAt(currentIndex, index)
+      moveDown(from: currentIndex)
+    }
+  }
+}
+
+let ve = readLine()!.split(separator: " ").map { Int($0)! }
+let v = ve[0], e = ve[1]
+let start = Int(readLine()!)! - 1
+var edges = Array(repeating: [(node: Int, weight: Int)](), count: v)
+
+for _ in 1...e {
+  let input = readLine()!.split(separator: " ").map { Int($0)! }
+  edges[input[0] - 1].append((input[1] - 1, input[2]))
+}
+
+var minHeap = Heap<(weight: Int, node: Int)>(comparer: <)
+var distances = Array(repeating: Int.max, count: v)
+
+minHeap.push((0, start))
+distances[start] = 0
+
+while let current = minHeap.pop() {
+  if distances[current.node] != current.weight { continue }
+  
+  for next in edges[current.node] {
+    guard distances[next.node] > distances[current.node] + next.weight else { continue }
+    
+    distances[next.node] = distances[current.node] + next.weight
+    minHeap.push((distances[next.node], next.node))
+  }
+}
+
+print(
+  distances.map {
+    $0 == .max ? "INF" : String($0)
+  }.joined(separator: "\n")
+)
